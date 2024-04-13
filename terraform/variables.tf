d87caa94b133322f10e0d82b8cef54e1c096aa2e
divijkulshrestha <divijk@gmail.com>
2024-04-13 14:42:13 +0000

    Terraform Files for GCP Resources

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  description = "GCP Project ID"
+  default     = "divij-zoomcamp-2024"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "asia-south1-c"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "ASIA-SOUTH1"
+}
+
+variable "bq_dataset_name" {
+  description = "Final Project - BigQuery Dataset"
+  default     = "gdelt2_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "GCS Bucket - Zoomcamp Practice"
+  default     = "divij-gdelt-2024-gcs-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
