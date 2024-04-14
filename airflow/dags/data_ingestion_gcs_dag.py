0b7d1885699acfe867da3194b76ee5f42e72dc43
divijkulshrestha <divijk@gmail.com>
2024-04-14 21:13:04 +0000

    Added Column Headers

--- airflow/dags/data_ingestion_gcs_dag.py
+++ airflow/dags/data_ingestion_gcs_dag.py
@@ -87,11 +87,18 @@ def read_last_hour_files():
         zip_ref.extract(lines[3].strip(), path=".")
     #print(lines[3])
 
+    column_names =['GLOBALEVENTID', 'SQLDATE', 'MonthYear', 'Year', 'FractionDate', 'Actor1Code', 'Actor1Name', 'Actor1CountryCode', 'Actor1KnownGroupCode', 
+    'Actor1EthnicCode', 'Actor1Religion1Code', 'Actor1Religion2Code', 'Actor1Type1Code', 'Actor1Type2Code', 'Actor1Type3Code', 'Actor2Code', 'Actor2Name', 
+    'Actor2CountryCode', 'Actor2KnownGroupCode', 'Actor2EthnicCode', 'Actor2Religion1Code', 'Actor2Religion2Code', 'Actor2Type1Code', 'Actor2Type2Code', 'Actor2Type3Code', 
+    'IsRootEvent', 'EventCode', 'EventBaseCode', 'EventRootCode', 'QuadClass', 'GoldsteinScale', 'NumMentions', 'NumSources', 'NumArticles', 'AvgTone', 'Actor1Geo_Type', 
+    'Actor1Geo_FullName', 'Actor1Geo_CountryCode', 'Actor1Geo_ADM1Code', 'Actor1Geo_ADM2Code', 'Actor1Geo_Lat', 'Actor1Geo_Long', 'Actor1Geo_FeatureID', 'Actor2Geo_Type', 
+    'Actor2Geo_FullName', 'Actor2Geo_CountryCode', 'Actor2Geo_ADM1Code', 'Actor2Geo_ADM2Code', 'Actor2Geo_Lat', 'Actor2Geo_Long', 'Actor2Geo_FeatureID', 'ActionGeo_Type', 
+    'ActionGeo_FullName', 'ActionGeo_CountryCode', 'ActionGeo_ADM1Code', 'ActionGeo_ADM2Code', 'ActionGeo_Lat', 'ActionGeo_Long', 'ActionGeo_FeatureID', 'DATEADDED', 'SOURCEURL']
 
     df_csv_append = pd.DataFrame()
              
     # concat the CSV files and generate a single parquet file
-    df_csv_concat = pd.concat([pd.read_csv(file.strip(), delimiter='\t') for file in lines], ignore_index=True)
+    df_csv_concat = pd.concat([pd.read_csv(file.strip(), delimiter='\t', header=None, names=column_names) for file in lines], ignore_index=True)
     df_csv_concat.to_parquet(source_pq, index=False)
    
 def str_to_date(datetime_str): 
