8df1537f2556906de4be5380dd6fdc4c89d058e2
divijkulshrestha <divijk@gmail.com>
2024-04-14 16:10:04 +0000

    Create DAG Tasks

--- airflow/dags/data_ingestion_gcs_dag.py
+++ airflow/dags/data_ingestion_gcs_dag.py
@@ -1,6 +1,13 @@
 import os
 import logging
 
+import time
+import datetime
+import zipfile
+import pandas as pd
+from datetime import datetime, timedelta
+from urllib.request import urlretrieve
+
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
@@ -8,36 +15,92 @@ from airflow.operators.python import PythonOperator
 
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
-from urllib.request import urlretrieve
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 BIGQUERY_DATASET = "gdelt2_dataset"
 
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
-
 
 last_url = "http://data.gdeltproject.org/gdeltv2/lastupdate.txt"
 file_name = "gdelt_data.txt"
-
+source_pq = "gdelt_data.parquet"
 
 def get_last_hour_files():
-    urlretrieve(last_url, f"{path_to_local_home}/{file_name}")
+    """
+    Function to retrieve the four most recent Events GDELT csv sourcefiles. 
+    """
+    urlretrieve(last_url, source_file)
+
+    f = open(source_file, "r")
+    lines = f.readlines()
+
+    #latest file
+    file0 = lines[0].split()[2]
+    timestamp0 = str_to_date(file0.split('/')[4].split('.')[0])
+    
+    #get previous hour files
+    timestamp1 = timestamp0 - timedelta(minutes=15)
+    timestamp2 = timestamp1 - timedelta(minutes=15)
+    timestamp3 = timestamp2 - timedelta(minutes=15)
+
+    file3 = f"http://data.gdeltproject.org/gdeltv2/{timestamp3.strftime('%Y%m%d%H%M%S')}.export.CSV.zip"
+    file2 = f"http://data.gdeltproject.org/gdeltv2/{timestamp2.strftime('%Y%m%d%H%M%S')}.export.CSV.zip"
+    file1 = f"http://data.gdeltproject.org/gdeltv2/{timestamp1.strftime('%Y%m%d%H%M%S')}.export.CSV.zip"
 
 
+    urlretrieve(file3, "file3.zip")
+    urlretrieve(file2, "file2.zip")
+    urlretrieve(file1, "file1.zip")
+    urlretrieve(file0, "file0.zip")
+    #print("Files downloaded!")
 
-def format_to_parquet(src_file):
-    if not src_file.endswith('.csv'):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+    with open(source_file, 'w') as file:
+        file.write(f"{timestamp0.strftime('%Y%m%d%H%M%S')}.export.CSV"+'\n')
+        file.write(f"{timestamp1.strftime('%Y%m%d%H%M%S')}.export.CSV"+'\n')
+        file.write(f"{timestamp2.strftime('%Y%m%d%H%M%S')}.export.CSV"+'\n')        
+        file.write(f"{timestamp3.strftime('%Y%m%d%H%M%S')}.export.CSV"+'\n')
+        
+def read_last_hour_files():
+    """
+    Function to extract the CSV files and generate a single source file.
+    """    
+    f = open(source_file, "r")
+    lines = f.readlines()
+
+    zip_file_path = "C:/Users/Anantk/AppData/Local/Programs/Python/Python311/"
+    df_csv_append = pd.DataFrame()
+
+    with zipfile.ZipFile(zip_file_path+'file0.zip', 'r') as zip_ref:
+        zip_ref.extract(lines[0].strip(), path=".")
+    #print(lines[0])
+
+    with zipfile.ZipFile(zip_file_path+'file1.zip', 'r') as zip_ref:
+        zip_ref.extract(lines[1].strip(), path=".")
+    #print(lines[1])
+
+    with zipfile.ZipFile(zip_file_path+'file2.zip', 'r') as zip_ref:
+        zip_ref.extract(lines[2].strip(), path=".")
+    #print(lines[2])
+
+    with zipfile.ZipFile(zip_file_path+'file3.zip', 'r') as zip_ref:
+        zip_ref.extract(lines[3].strip(), path=".")
+    #print(lines[3])
 
 
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
+    df_csv_append = pd.DataFrame()
+             
+    # concat the CSV files and generate a single parquet file
+    df_csv_concat = pd.concat([pd.read_csv(file.strip(), delimiter='\t') for file in lines], ignore_index=True)
+    df_csv_concat.to_parquet(source_pq, index=False)
+   
+def str_to_date(datetime_str): 
+    datetime_object = datetime.strptime(datetime_str, '%Y%m%d%H%M%S')
+    return datetime_object
+    
+def remove_file(filename):
+    os.remove(filename)
+
 def upload_to_gcs(bucket, object_name, local_file):
     """
     Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
@@ -69,8 +132,9 @@ default_args = {
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
     dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
+    schedule_interval="@hourly",
     default_args=default_args,
+    description="DAG to lookup GDELT past hour files and load to GCS Bucket as Parquet File",
     catchup=False,
     max_active_runs=1,
     tags=['dtc-de'],
@@ -78,25 +142,21 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        python_callable=get_last_hour_files
     )
 
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-        },
+    prepare_parquet_task = PythonOperator(
+        task_id="prepare_parquet_task",
+        python_callable=read_last_hour_files
     )
 
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"raw/{source_pq}",
+            "local_file": source_pq,
         },
     )
 
@@ -110,9 +170,9 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/raw/{source_pq}"],
             },
         },
     )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task >> prepare_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
